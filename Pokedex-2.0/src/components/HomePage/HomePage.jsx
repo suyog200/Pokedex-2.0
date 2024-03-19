c2c2b616b79939e63dade05aff93029c058a7a9c
suyog200 <suygchari@gmail.com>
2024-03-19 12:47:11 +0530

    pokdedex comp - adding search bar (search pokemon feature)

--- Pokedex-2.0/src/components/HomePage/HomePage.jsx
+++ Pokedex-2.0/src/components/HomePage/HomePage.jsx
@@ -1,37 +1,63 @@
-import { useRef, useEffect } from 'react';
-import './HomePage.css'
-import HomeVideo from '../../assets/images/home-main.mp4'
-import DownArrow from '../../assets/images/go-down-arrow.png'
-import Navbar from '../Navbar/Navbar';
+import { useRef, useEffect } from "react";
+import "./HomePage.css";
+import HomeVideo from "../../assets/images/home-main.mp4";
+import DownArrow from "../../assets/images/go-down-arrow.png";
+import Navbar from "../Navbar/Navbar";
+import PokdexHeadImg from '../../assets/images/pokedex-font.png'
+import PokedexLogo from '../../assets/images/logo2.png'
 
 export default function HomePage() {
-    const videoRef = useRef(null);
+  const videoRef = useRef(null);
 
-    useEffect(() => {
-        if(videoRef.current) {
-            videoRef.current.playbackRate = 0.6;
-        }
-    }, [])
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.6;
+    }
+  }, []);
 
   return (
-    <div id='home-container'>
+    <>
+    {/* home page */}
+      <section id="home-container">
         <Navbar />
-        <video 
-        autoPlay 
-        loop
-        muted
-        src={HomeVideo} 
-        className='bgVideo'
-        ref={videoRef}
+        <video
+          //autoPlay
+          //loop
+          // muted
+          src={HomeVideo}
+          className="bgVideo"
+          ref={videoRef}
         ></video>
-        <div className='home-content--left'>
-            <div className='main-heading'>
-                <p className='heading1'>Welcome to Pokemon World</p>
-                <p className='heading2'>where you can catch them all</p>
-            </div>
-                <button className='btn-grab'>Grab your Pokemons</button>
+        <div className="home-content--left">
+          <div className="main-heading">
+            <p className="heading1">Welcome to Pokemon World</p>
+            <p className="heading2">where you can catch them all</p>
+          </div>
+          <button className="btn-grab">Grab your Pokemons</button>
         </div>
-        <img src={DownArrow} alt="" className='down-arrow'/>
-    </div>
+        <img src={DownArrow} alt="" className="down-arrow" />
+      </section>
+      {/* pokedex */}
+      <section id="pokedex-container">
+        <div className="pokedex-header">
+          <img src={PokdexHeadImg} alt="" className="title-img"/>
+          <img src={PokedexLogo} alt="" className="bottom-img"/>
+        </div>
+        <div className="searchBar--container">
+          <div className="searchBar-left">
+            <input
+              type="text"
+              placeholder="Search Pokemon"
+              className="searchBar-input"
+            />
+            <button className="btn">Search</button>
+          </div>
+          <div className="searchBar-right">
+            <button className="btn">Favroite</button>
+            <button className="btn">Limit</button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
