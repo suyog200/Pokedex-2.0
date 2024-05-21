98874088bb826d2305c9fc7fe24eb59e927a23be
suyog200 <suygchari@gmail.com>
2024-05-21 18:51:30 +0530

    added paralax effect for home img.

--- Pokedex-2.0/src/components/HomePage/HomePage.jsx
+++ Pokedex-2.0/src/components/HomePage/HomePage.jsx
@@ -3,10 +3,14 @@ import "./HomePage.css";
 import HomeVideo from "../../assets/images/home-main.mp4";
 import DownArrow from "../../assets/images/go-down-arrow.png";
 import Navbar from "../Navbar/Navbar";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function HomePage() {
   const videoRef = useRef(null);
+  const {scrollY} = useScroll();
+
+  const scaleImg = useTransform(scrollY, [0, 500, 800], [1, 1.5,2]);
+  const opacityImg = useTransform(scrollY, [500, 600,800], [1, 0.5, 0]);
 
   useEffect(() => {
     if (videoRef.current) {
@@ -19,14 +23,15 @@ export default function HomePage() {
       {/* home page */}
       <section id="home-container">
         <Navbar />
-        <video
+        <motion.video
           autoPlay
           loop
           muted
           src={HomeVideo}
           className="bgVideo"
           ref={videoRef}
-        ></video>
+          style={{scale: scaleImg, opacity: opacityImg}}
+        ></motion.video>
         <div className="home-content--left">
           <div className="main-heading">
             <p className="heading1">Welcome to Pokemon World</p>
